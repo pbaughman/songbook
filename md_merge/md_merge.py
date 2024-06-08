d7df52e60df3e1056f710a5dac74676e528c1e6e
Pete Baughman <4884343+pbaughman@users.noreply.github.com>
2024-06-08 12:01:13 -0700

    Add --include argument to md_merge tool

--- md_merge/md_merge.py
+++ md_merge/md_merge.py
@@ -7,18 +7,30 @@ def parse_args(argv):
   parser = argparse.ArgumentParser()
   parser.add_argument('-t', '--template', required=True)
   parser.add_argument('-o', '--output', required=True)
+  parser.add_argument('-i', '--include', action='append')
   return parser.parse_args(argv)
+
+def find_include(file_name, include_files):
+  """Finds a file reverenced in the template by searching al included files.
+
+  Returns:
+    A file path like "/path/to/to_include.md"
+  """
+  for artifact_path in include_files:
+    if artifact_path.endswith(file_name):
+      return artifact_path
+  raise FileNotFoundError(
+      f"Failed to find {file_name} in any of {include_files}"
+      )
 
 def main(argv):
   args = parse_args(argv)
-  print(os.getcwd())
-  infile_dir = os.path.dirname(args.template)
   with open(args.template, 'r') as rfile:
     infile = rfile.read()
   outfile = infile
   for include in re.findall(r'{{.+}}', infile):
     include_file = include.strip('{}')
-    with open(os.path.join(infile_dir, include_file), 'r') as content:
+    with open(find_include(include_file, args.include), 'r') as content:
       outfile = outfile.replace(include, content.read())
 
   with open(args.output, 'w') as wfile:
